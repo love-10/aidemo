a20e10db76591180b8651caf48e14f6a5ce86867
wilyn <anizwel99@gmail.com>
2024-06-20 21:14:10 +0800

    11

---
+++ aidemo/app/app/main/cpp/3rdparty/base-utils/test/cmake-build-debug/CMakeFiles/openmp_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openmp_test.dir/openmp_test.cpp.o"
+  "openmp_test"
+  "openmp_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openmp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ aidemo/app/app/main/cpp/3rdparty/base-utils/test/cmake-build-debug/CMakeFiles/stereo_match_sgbm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stereo_match_sgbm.dir/opencv/stereo_match_sgbm.cpp.o"
+  "stereo_match_sgbm"
+  "stereo_match_sgbm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stereo_match_sgbm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
